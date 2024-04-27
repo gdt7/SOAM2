7af25980e60642f0bd4fb98cf4e83bb88e96fa20
leonel cespedes <ldc.mail2@gmail.com>
2024-04-27 00:37:05 -0300

    Cambios timeout, funciones de maquina de estado, pulsador

--- sketch.ino
+++ sketch.ino
@@ -1,4 +1,3 @@
-
 #include <ESP32Servo.h>
 #include <HardwareSerial.h>
 #include <SPI.h>
@@ -22,7 +21,7 @@ LiquidCrystal lcd(22, 23, 15, 34, 35, 33);
 #define BUZZER 21
 #define SS_PIN 5   // Pin SS (Slave Select) del lector RFID
 #define RST_PIN 16 // Pin de reinicio del lector RFID
-#define UMBRAL_DIFERENCIA_TIMEOUT 50
+#define UMBRAL_DIFERENCIA_TIMEOUT 5000 //5 segundos
 #define UMBRAL_DISTANCIA_CM 50
 
 
@@ -47,6 +46,11 @@ Servo Servo1;
 float duration_us, distance_cm;
 bool timeout;
 long lct;
+long tiempoDesde;
+long tiempoDesde2;
+int ban = 0;
+int ban2 = 0;
+int val = 0;
 
 // Definición de los estados
 enum estados
@@ -80,6 +84,7 @@ bool verificarPulsador();
 bool verificarRIFD();
 bool verificarSensorProximidad();
 void log(String mensaje);
+bool stimeout();
 
 void none()
 {
@@ -97,24 +102,79 @@ void pasar_a_idle()
 
 void pasar_a_barrera_abierta()
 {
-  moverServo(ANGULO_PULSADO);
-  // digitalWrite(PIN_PULSADOR, HIGH);
-  digitalWrite(LED, HIGH);
-  digitalWrite(BUZZER, HIGH);
-  estado_actual = ST_BARRERA_ABIERTA;
+    moverServo(ANGULO_PULSADO);
+    // digitalWrite(PIN_PULSADOR, HIGH);
+    digitalWrite(LED, HIGH);
+    digitalWrite(BUZZER, HIGH);
+    estado_actual = ST_BARRERA_ABIERTA;
+
+    if(nuevo_evento == EV_PULSADOR){
+      Serial.println("Se pasa de PULSADOR a CONTINUAR");
+      nuevo_evento = EV_CONTINUAR;
+      return;
+    }
+
+    //Serial.println("Barrera abierta y distancia!");
+
+    
+    /*
+    if(ban2==0){
+      Serial.println("Primera vez que calcula tiempoDesde en barrera abierta");
+      tiempoDesde2 = millis();
+    }
+
+    if (stimeout(UMBRAL_DIFERENCIA_TIMEOUT)) {
+      ban2=0;
+      Serial.println("Han pasado 5 segundos! Se cierra la barrera en caso de no detectar nada!");
+      nuevo_evento = EV_TIMEOUT;
+      pasar_a_idle();
+    }
+      ban2=1;*/
+    
 }
+
 
 void pasar_a_esperando_respuesta()
 {
   estado_actual = ST_ESPERANDO_RESPUESTA;
+
+    if(nuevo_evento == EV_PULSADOR){
+      nuevo_evento = EV_CONTINUAR;
+      return;
+    }
+
+    //Serial.print("BAN: ");
+    //Serial.println(ban);
+  if(ban==0){
+    //Serial.println("Primera vez que calcula tiempoDesde");
+    tiempoDesde = millis();
+  }
+
+  if (stimeout(UMBRAL_DIFERENCIA_TIMEOUT)) {//|| verificarRIFD()) {
+    ban=0;
+    Serial.println("Han pasado 5 segundos y NO fue autorizado, se retorna al estado IDLE");
+    nuevo_evento = EV_TIMEOUT;
+    pasar_a_idle();
+  }else{
+
+      if(verificarRIFD()){
+        Serial.println("Camion autorizado!");
+        pasar_a_barrera_abierta();
+        return;
+      }
+
+    ban=1;
+  }
+
+    
 }
 
 typedef void (*transition)();
 transition state_table[MAX_ESTADOS][MAX_EVENTOS] =
     {
-        {pasar_a_barrera_abierta, none, pasar_a_esperando_respuesta, none, none, pasar_a_barrera_abierta, none}, // state ST_IDLE
-        {none, pasar_a_idle, none, pasar_a_idle, pasar_a_barrera_abierta, none, none},                           // state ST_ESPERANDO_RESPUESTA
-        {pasar_a_idle, pasar_a_idle, none, none, none, none, none}                                       // state ST_BARRERA_ABIERTA
+        {pasar_a_barrera_abierta, none, pasar_a_esperando_respuesta, none, none, none, pasar_a_idle},//state ST_IDLE
+        {none, pasar_a_idle, pasar_a_esperando_respuesta, pasar_a_idle, pasar_a_barrera_abierta, pasar_a_esperando_respuesta, pasar_a_esperando_respuesta},//state ST_ESPERANDO_RESPUESTA
+        {pasar_a_idle, pasar_a_idle, none, none, none, pasar_a_idle, pasar_a_barrera_abierta} //state ST_BARRERA_ABIERTA
 };
 // EVENTOS {"EV_PULSADOR", "EV_TIMEOUT", "EV_LEER_RIFD", "EV_NO_AUTORIZADO", "EV_AUTORIZADO", "EV_DISTANCIA", "EV_CONTINUAR"};
 /**********************************************************************************************/
@@ -169,21 +229,26 @@ void start()
   {
   }
   pinMode(BUZZER, OUTPUT);
-  lcd.begin(16, 2);
+  //lcd.begin(16, 2);
   timeout = false;
-  lct = millis();
+  lct = millis(); // Guarda el tiempo actual al inicio
+  Serial.print("LCT: ");
+  Serial.println(lct);
 }
 
 void fsm()
 {
   tomar_evento();
+  Serial.println("ESTADO ACTUAL: " + estados_string[estado_actual]);
+  Serial.println("EVENTO: " + eventos_string[nuevo_evento]);
+  
 
   if(nuevo_evento >= 0 && nuevo_evento < MAX_EVENTOS && estado_actual >= 0 && estado_actual < MAX_ESTADOS)
   {
-    if( nuevo_evento != EV_CONTINUAR )
+    /*if( nuevo_evento != EV_CONTINUAR )
     {
       //DebugPrintEstado(states_s[estado_actual], events_s[nuevo_evento]);
-    }
+    }*/
     state_table[estado_actual][nuevo_evento]();
   } else 
   {
@@ -191,27 +256,34 @@ void fsm()
     //DebugPrintEstado(estados_string[ST_ERROR], eventos_string[EV_UNKNOW]);
   }
 
-  nuevo_evento   = EV_CONTINUAR;
+  //nuevo_evento   = EV_CONTINUAR;
 }
 
 void tomar_evento()
 {
-  long ct = millis();
-  int diferencia = (ct - lct);
-  timeout = (diferencia > UMBRAL_DIFERENCIA_TIMEOUT) ? (true) : (false);
+  /*long ct = millis();
+  int diferencia = (ct - lct); 
+  Serial.print("CT: ");
+  Serial.println(ct);
 
+  Serial.print("DIFERENCIA: ");
+  Serial.println(diferencia);
+
+  timeout = (diferencia > UMBRAL_DIFERENCIA_TIMEOUT) ? (true) : (false);
+  verificarSensorProximidad(); //ver
   if (timeout)
   {
     timeout = false;
     lct = ct;
-
-    if(verificarSensorProximidad() || verificarPulsador() || verificarRIFD())
+  */
+    if(verificarSensorProximidad() || verificarPulsador())
     {
       return;
     }
-  }
+  //}
 
-  nuevo_evento = EV_CONTINUAR;
+  //nuevo_evento = EV_CONTINUAR;
+  //nuevo_evento = EV_DISTANCIA;
 }
 
 void moverServo(int angulo)
@@ -222,14 +294,24 @@ void moverServo(int angulo)
 
 bool verificarPulsador()
 {
-  if (digitalRead(PIN_PULSADOR) == HIGH)
+  
+
+  if (digitalRead(PIN_PULSADOR) == HIGH && val == 0)
   {
+    Serial.print("PULSADOR ENCENDIDO!");
+    //nuevo_evento = EV_PULSADOR;
     nuevo_evento = EV_PULSADOR;
+    val = 1;
     return true;
   }
-  else
+  else if(digitalRead(PIN_PULSADOR) == LOW )
   {
+    Serial.print("PULSADOR APAGADO!");
+    //nuevo_evento = EV_CONTINUAR;
+    val = 0;
     return false;
+  }else{
+    return true;
   }
 }
 
@@ -254,15 +336,24 @@ bool verificarSensorProximidad()
 {
   sensores[SENSOR_PROXIMIDAD].valor_actual = leerSensorDistancia();
 
+   Serial.print("DISTANCIA: ");
+   Serial.println(leerSensorDistancia());
+
   float valor_actual = sensores[SENSOR_PROXIMIDAD].valor_actual;
 
-  if(valor_actual < UMBRAL_DISTANCIA_CM)
+  if(valor_actual < UMBRAL_DISTANCIA_CM) //si la distancia es menos de 50
   {
-    nuevo_evento = EV_DISTANCIA;
+    nuevo_evento = EV_LEER_RIFD; //detecto algo para leer
     return true;
+  }else{
+
+      if(nuevo_evento != EV_CONTINUAR){ //nuevo_evento != EV_CONTINUAR && 
+        nuevo_evento = EV_DISTANCIA;
+      }
+    
+    return false;
   }
 
-  return false;
 }
 
 bool verificarRIFD()
@@ -271,11 +362,41 @@ bool verificarRIFD()
   sensores[SENSOR_RFID].valor_actual = false;
 
   bool valor_actual = sensores[SENSOR_RFID].valor_actual;
-
+  /****************************************************************/
+  valor_actual = true; //HARCODEO VALOR TRUE (CAMION AUTORIZADO)
   if(valor_actual)
-  {
-    nuevo_evento = EV_LEER_RIFD;
+  { 
+    Serial.print("Verificar RFID: TRUE \n");
+    nuevo_evento = EV_AUTORIZADO;
     return true;
   }
+  Serial.print("Verificar RFID: FALSE \n");
+  nuevo_evento = EV_NO_AUTORIZADO;
   return false;
+}
+
+
+bool stimeout(unsigned long intervalo) {
+  /****************************/ 
+    Serial.print("tiempo desde: ");
+    Serial.println(tiempoDesde);
+
+    long tiempoHastaAhora = millis(); //desde que se inicio el programa
+    Serial.print("tiempo hasta ahora: ");
+    Serial.println(tiempoHastaAhora);
+
+    long tiempoQuePaso = tiempoHastaAhora - tiempoDesde;
+    Serial.print("tiempo que paso: ");
+    Serial.println(tiempoQuePaso);
+
+    if(tiempoQuePaso > intervalo){
+      Serial.println("Retorna TRUE");
+        return true;
+    }else{
+      Serial.println("Retorna FALSE");
+        return false;
+    }
+  /****************************/ 
+  //return (millis() - lct >= intervalo);
+
 }
