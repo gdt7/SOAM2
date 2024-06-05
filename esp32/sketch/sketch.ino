8aaa851c5f148355d7a7566f58549fd423004dc4
Lautaro <dercolelautaro@gmail.com>
2024-06-04 21:05:45 -0300

    Cambio prioridad

--- esp32/sketch/sketch.ino
+++ esp32/sketch/sketch.ino
@@ -51,7 +51,7 @@ TaskHandle_t Task1;
 
 #define CORE_ZERO 0
 #define TASK_STACK_SIZE 1000
-#define TASK_PRIORITY 1
+#define TASK_PRIORITY -1
 #define D_NOTA 4
 #define D_MEDIA_NOTA 8
 
@@ -133,11 +133,11 @@ BluetoothSerial SerialBT;
 
 typedef void (*transition)();
 transition state_table[MAX_ESTADOS][MAX_EVENTOS] =
-{
-    {none, pasar_a_barrera_abierta_m, none, pasar_a_esperando_respuesta, none, none, none},//state ST_IDLE
-    {none, none, pasar_a_idle, none, pasar_a_idle, pasar_a_barrera_abierta, none},//state ST_ESPERANDO_RESPUESTA
-    {none, pasar_a_idle, pasar_a_idle, none, none, none, pasar_a_idle}, //state ST_BARRERA_ABIERTA
-    {none, pasar_a_idle, pasar_a_idle, none, none, none, none } //state ST_BARRERA_ABIERTA_MANUAL
+    {
+        {none, pasar_a_barrera_abierta_m, none, pasar_a_esperando_respuesta, none, none, none},//state ST_IDLE
+        {none, none, pasar_a_idle, none, pasar_a_idle, pasar_a_barrera_abierta, none},//state ST_ESPERANDO_RESPUESTA
+        {none, pasar_a_idle, pasar_a_idle, none, none, none, pasar_a_idle}, //state ST_BARRERA_ABIERTA
+        {none, pasar_a_idle, pasar_a_idle, none, none, none, none } //state ST_BARRERA_ABIERTA_MANUAL
 };
 // EVENTOS {"EV_CONTINUAR", "EV_PULSADOR", "EV_TIMEOUT", "EV_LEER_RFID", "EV_NO_AUTORIZADO", "EV_AUTORIZADO", "EV_DISTANCIA"};
 /**********************************************************************************************/
